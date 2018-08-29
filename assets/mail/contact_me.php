111796d8779223dbb2353e04cffcf8e87ab62f16
Heath <palmerha@mail.uc.edu>
2018-08-29 14:43:16 -0400

    Nirmalya's Update

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -15,10 +15,10 @@ $message = $_POST['comments'];
 
 	
 // Create the email and send the message
-$to = 'info@hyperloopuc.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'heath.palmer@hyperloopuc.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Enquiry from website from $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: info@hyperloopuc.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: heath.palmer@hyperloopuc.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 $thankYou="<p>Thank you! Your message has been sent.</p>";
